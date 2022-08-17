381a28ad818cc4936be0278adc6c71f61c722b88
Tetraslam <bhowmickshresht@gmail.com>
2022-08-17 13:06:43 +0530

    added medication events

--- main.py
+++ main.py
@@ -8,15 +8,30 @@ def addEvent():
   eventfile = open(eventname + ".txt", "w+")
   eventcontent = input("Please enter event details: ")
   eventfile.write(eventcontent)
-  
-def main():
-  with mic as source:
-    r.adjust_for_ambient_noise(source)
-    audio = r.listen(source)
-    speech = r.recognize_google(audio)
-    print(speech)
-    if speech == "add event" or speech == "add an event" or speech == "new event":
-      addEvent()
 
+def addMedication():
+  medicationname = input("Please enter medication name: ")
+  medicationfile = open(medicationname + ".txt", "w+")
+  medicationcontent = input("Medication details (volume/number of pills, time to take them)")
+  medicationfile.write(medicationcontent)
+
+def main():
+  option = input("Add event or medication?")
+  if option=="event" or option=="add event":
+    with mic as source:
+      r.adjust_for_ambient_noise(source)
+      audio = r.listen(source)
+      speech = r.recognize_google(audio)
+      print(speech)
+      if speech == "add event" or speech == "add an event" or speech == "new event":
+        addEvent()
+  elif option=="medication" or option=="add medication":
+    with mic as source:
+      r.adjust_for_ambient_noise(source)
+      audio = r.listen(source)
+      speech = r.recognize_google(audio)
+      print(speech)
+      if speech == "add medication" or speech == "add a medication" or speech == "new medication":
+        addMedication()
 if __name__=="__main__":
   main()
